c5a531e49ebda05b738d94a7d63a189418d152da
Pavel Sušický <pavels.sus@gmail.com>
2021-05-19 23:23:11 +0200

    chore: enhanced build script

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,9 +1,31 @@
-var dev = process.env.NODE_ENV === "development"
+(async () => {
+   let { build } = require("esbuild")
 
-console.log(dev);
+   let dev = process.env.NODE_ENV === "development"
 
-require("esbuild").build({
-   entryPoints: ["src/code.ts"],
-   bundle: true,
-   outfile: "out/code.js"
-});
+   let cleanPlugin = {
+      name: "clean",
+      setup(build) {
+         let fs = require("fs");
+
+         try {
+            fs.rmdirSync("out", { recursive: true });
+         } catch (error) {
+            console.error(error);
+         }
+      }
+   }
+
+   try {
+      await build({
+         entryPoints: ["src/code.ts"],
+         sourcemap: dev,
+         minify: !dev,
+         bundle: true,
+         outfile: "out/code.js",
+         plugins: [cleanPlugin]
+      });
+   } catch (error) {
+      console.error(error)
+   }
+})()
