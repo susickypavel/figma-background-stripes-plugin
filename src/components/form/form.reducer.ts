1646bc5f7e7a3d6605939813f5f35f1d62b903b9
Pavel Sušický <pavels.sus@gmail.com>
2021-05-22 19:44:18 +0200

    chore: added eslint base configuration

--- src/components/form/form.reducer.ts
+++ src/components/form/form.reducer.ts
@@ -44,9 +44,13 @@ export function reducer(state: State, action: Action): State {
         { pluginMessage: { type: "create-background", ...state } },
         "*"
       );
+
+      return state;
     }
     case "CANCEL": {
       parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+
+      return state;
     }
     default:
       return state;
